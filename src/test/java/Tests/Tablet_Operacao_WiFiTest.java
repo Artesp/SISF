8c734cea46c6e074a8ae312296491a0595bb31b0
rafael.silva <cap.artesp@gmail.com>
2018-11-13 19:00:15 -0200

    Tablet-Operacao-WiFi - automatizados 4496 à 4498

--- src/test/java/Tests/Tablet_Operacao_WiFiTest.java
+++ src/test/java/Tests/Tablet_Operacao_WiFiTest.java
@@ -7,10 +7,11 @@ import Pages.LoginPage;
 import Pages.ModulosPage;
 import Pages.Tablet_Operacao_WiFiPage;
 import Pages.Tablet_Operacao_WiFi_AddMedicaoPage;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.omg.CORBA.Object;
 import org.openqa.selenium.By;
 
 public class Tablet_Operacao_WiFiTest extends BaseTest {
@@ -22,14 +23,14 @@ public class Tablet_Operacao_WiFiTest extends BaseTest {
 
     @Test
     @DisplayName("MTM_ID 4505: FP13/RVN3 - Verificar número máximo de medições na seção Wi-Fi.")
-    /*@Ignore*/
+    @Ignore
     public void inserirMedicao_EmLote(){
         preparaCenario();
         preencherFiscalizacao_MedicaoEmLote();
         salvar();
 
         String expected = EXPECTEDS.WIFI.toString();
-        Assert.assertEquals(expected, obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:comp/lstfsc_grupo")));
+        assertEquals(expected, obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:comp/lstfsc_grupo")));
     }
 
     @Test
@@ -40,10 +41,62 @@ public class Tablet_Operacao_WiFiTest extends BaseTest {
         salvar();
 
         String expected = EXPECTEDS.WIFI.toString();
-        Assert.assertEquals(expected, obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:comp/lstfsc_grupo")));
+        assertEquals(expected, obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:comp/lstfsc_grupo")));
 
         enviar(3000);
     }
+
+    @Test
+    @DisplayName("MTM_ID 4596: FP2 - Verificar tela principal da fiscalização de Wi-Fi")
+    public void verificarMenuLateralFiscalizacao_Operacao_WiFi(){
+        preparaCenario();
+
+        String expecteds[] = new String [4];
+        expecteds[0] = EXPECTEDS.RODOVIA.toString();
+        expecteds[1] = EXPECTEDS.WIFI.toString();
+        expecteds[2] = EXPECTEDS.OBS_FISCALIZACAO.toString();
+        expecteds[3] = EXPECTEDS.GALERIA.toString();
+
+        String indexMenu [] = new String [4];
+        indexMenu[0] = MENU_WIFI.MENUSISF_RODOVIA.toString();
+        indexMenu[1] = MENU_WIFI.MENUSISF_WIFI.toString();
+        indexMenu[2] = MENU_WIFI.MENUSISF_OBS_FISCALIZACAO.toString();
+        indexMenu[3] = MENU_WIFI.MENUSISF_GALERIA.toString();
+
+        for(int i = 0; i < expecteds.length; i++){
+            String index = indexMenu[i];
+            String elemento = expecteds[i];
+            String path = "//android.widget.LinearLayout[@resource-id='br.gov.sp.artesp.sisf.mobile:comp/fsc_content']/android.widget.Button[@index='" + index + "']";
+            String nomeMenu = obterTextoElemento(By.xpath(path));
+            assertEquals(elemento, nomeMenu);
+        }
+
+    }
+
+    @Test
+    @DisplayName("MTM_ID 4497: FP3/FP6 - Verificar seção Wi-Fi - Botão Medir")
+    public void verificarBotaoMedir_Operacao_WiFi(){
+        preparaCenario();
+        navegarMenuPrincipal(MENU_WIFI.MENUSISF_WIFI.toString());
+        assertsBotoes_WiFi_Medicao();
+
+        medicaoPage.clicarBotaoIntensidadeSinal();
+        assertEquals("-127", obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:id/intensidade_sinal")));
+    }
+
+    @Test
+    @DisplayName("MTM_ID 4498: FP7/FP10 - Verificar seção Wi-Fi - Botão Relógio")
+    public void verificarBotaoCronometro_Operacao_WiFi(){
+        preparaCenario();
+        navegarMenuPrincipal(MENU_WIFI.MENUSISF_WIFI.toString());
+        assertsBotoes_WiFi_Medicao();
+
+        medicaoPage.clicarBotaoCronometro();
+        esperaCarregar(1000);
+        medicaoPage.clicarBotaoFinalizaCronometro();
+        assertEquals("00:00:01", obterTextoElemento(By.id("br.gov.sp.artesp.sisf.mobile:id/duracao_chamada")));
+    }
+
 
     private void preparaCenario() {
         loginPage.realizaLogin();
@@ -78,6 +131,16 @@ public class Tablet_Operacao_WiFiTest extends BaseTest {
 
         navegarMenuPrincipal(MENU_WIFI.MENUSISF_GALERIA.toString());
         page.capturarImagem();
+    }
+
+    private void assertsBotoes_WiFi_Medicao(){
+        boolean elemento = elementoExiste(By.id("br.gov.sp.artesp.sisf.mobile:id/lstcbn_btn_add"));
+        assertTrue(elemento);
+        page.clicarBotaoAddMedicao();
+        elemento = elementoExiste(By.id("br.gov.sp.artesp.sisf.mobile:id/btn_intensidade_sinal"));
+        assertTrue(elemento);
+        elemento = elementoExiste(By.id("br.gov.sp.artesp.sisf.mobile:id/btn_cronometro"));
+        assertTrue(elemento);
     }
 
 
