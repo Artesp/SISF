b5391ef297b3b6728a4db4213ac1b1ecdb8b252f
artesp <cap.artesp@gmail.com>
2019-07-25 15:49:51 -0300

    Documentando Serviço Local para inicializar o Appium pelo Código.

--- src/test/java/Core/DriverFactory.java
+++ src/test/java/Core/DriverFactory.java
@@ -1,6 +1,9 @@
 package Core;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -11,9 +14,13 @@ public class DriverFactory {
 
     private static AndroidDriver driver;
 
+//    private static AppiumDriverLocalService service;
+//    private static AppiumServiceBuilder builder;
+
     public static AndroidDriver getDriver(){
         if (driver == null) {
             createDriver();
+            //service.start();
         }
         return driver;
     }
@@ -29,13 +36,25 @@ public class DriverFactory {
         capacidade.setCapability("resetKeyboard", true);
         capacidade.setCapability("newCommandTimeout", 300);
 
+        //Build the AppiumService
+//        builder = new AppiumServiceBuilder();
+//        builder.withIPAddress("127.0.0.1");
+//        builder.usingPort(4723);
+//        builder.withCapabilities(capacidade);
+//        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+//        builder.withArgument(GeneralServerFlag.LOG_LEVEL,"error");
+//
+//        //Start server with the builder
+//        service = AppiumDriverLocalService.buildService(builder);
+
         try {
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidade);
             //driver = new AndroidDriver(new URL("http://10.23.42.141:4723/wd/hub"), capacidade);
-        } catch (MalformedURLException e) {
+            //driver = new AndroidDriver(service.getUrl(), capacidade);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        driver.hideKeyboard();
+        //driver.hideKeyboard();
         driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
 
     }
@@ -44,6 +63,7 @@ public class DriverFactory {
         if (driver != null) {
             driver.quit();
             driver=null;
+            //service.stop();
         }
     }
 }
